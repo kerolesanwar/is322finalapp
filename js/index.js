5616cf73219c74e8143d7d7497b59a2f683d8ca7
Hector Castillo <hector@hector-VirtualBox.(none)>
2014-05-06 15:00:48 -0400

    added camera

--- js/index.js
+++ js/index.js
@@ -132,3 +132,30 @@ function PhotoLibraryControl(controlDiv, map) {
 }
 
 	google.maps.event.addDomListener(window, 'load', initialize);
+
+/**
+Camera
+**/
+
+document.addEventListener("deviceready",onDeviceReady,false);
+function onDeviceReady() {
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+    }
+    
+    function onPhotoDataSuccess(imageData) {
+      // Uncomment to view the base64 encoded image data
+      //alert(imageData);
+
+      
+    }
+    
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    }
+    
+        function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
+    }
